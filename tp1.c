1e231c3de725264b47a272b41c6164d9dfdc0196
ricardobacano <117918290+ricardobacano@users.noreply.github.com>
2024-03-14 14:18:46 -0300

    Add files via upload

---
+++ tp1.c
@@ -0,0 +1,25 @@
+/* Arquivo MAIN que usa o TAD racionais */
+
+/* coloque seus includes aqui */
+
+#include <stdio.h> 
+#include <stdlib.h>
+#include "racionais.h"
+
+int main () {
+
+    srand(0);
+
+    int n;
+    scanf("%d", &n);
+
+    int MAX;
+    scanf("%d", &MAX);
+
+    for (int i = 1; i <= n; i++) {
+        
+    }
+
+
+        return 0;
+}
